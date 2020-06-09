9e96fae0f2a389009603cb6c3b30e1ece10c107b
Hemn Movafeghi <hemn791@gmail.com>
2020-06-09 12:35:23 +0300

    basic registraion form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,14 +6,47 @@
         <img src="img/weblogo.png" alt="">
     </a>
     <div class="form-content form-account">
-        <input type="text" class="txt" placeholder="نام و نام خانوادگی">
-        <input type="text" class="txt txt-l" placeholder="ایمیل">
-        <input type="text" class="txt txt-l" placeholder="شماره موبایل">
-        <input type="text" class="txt txt-l" placeholder="رمز عبور">
-        <span class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
-        <br>
-        <button class="btn continue-btn">ثبت نام و ادامه</button>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <input type="text" class="txt @error('name') is-invalid @enderror" placeholder="نام و نام خانوادگی"
+                   name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+            >
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
+            <input id="email" type="email" class="txt txt-l @error('email') is-invalid @enderror" placeholder="ایمیل"
+                   name="email" value="{{ old('email') }}" required autocomplete="email"
+            >
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+            @enderror
+
+
+            <input id="password" type="password" class="txt txt-l @error('password') is-invalid @enderror"
+                   placeholder="رمز عبور"
+                   name="password" required autocomplete="new-password"
+            >
+            <input  id="password-confirm" type="password" class="txt txt-l @error('password') is-invalid @enderror"
+                    placeholder="تایید رمز عبور"
+                    name="password_confirmation" required autocomplete="new-password"
+            >
+            <span class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
+            @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
+
+
+            <br>
+            <button class="btn continue-btn">ثبت نام و ادامه</button>
+        </form>
     </div>
     <div class="form-footer">
         <a href="login.html">صفحه ورود</a>

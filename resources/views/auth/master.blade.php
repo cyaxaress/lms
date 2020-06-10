e9637c4b218baeb626c998e3944df29f4c988902
Hemn Movafeghi <hemn791@gmail.com>
2020-06-10 18:32:58 +0300

    Reset password functionality add

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/font/font.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/font/font.css">
     <title>صفحه ثبت نام</title>
 </head>
 <body>

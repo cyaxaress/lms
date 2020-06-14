c39e636cb36ee15aaed5b55ce0e25801436adeee
Hemn Movafeghi <hemn791@gmail.com>
2020-06-14 16:07:03 +0300

    Set auth.provider.users.model from UserSerivceProvider

--- modules/Cyaxaress/User/Providers/UserServiceProvider.php
+++ modules/Cyaxaress/User/Providers/UserServiceProvider.php
@@ -4,12 +4,18 @@
 namespace Cyaxaress\User\Providers;
 
 
+use Cyaxaress\User\Models\User;
 use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        config()->set('auth.providers.users.model', User::class);
+    }
     public function boot()
     {
+
         $this->loadRoutesFrom(__DIR__ . '/../Routes/user_routes.php');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
         $this->loadFactoriesFrom(__DIR__ . '/../Database/Factories');

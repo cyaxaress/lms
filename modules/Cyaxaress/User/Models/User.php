7d7d004ee738975322e5836d6d27ac6528627671
Hemn Movafeghi <hemn791@gmail.com>
2020-06-20 13:35:56 +0300

    Create VerifyEmail Notification

--- modules/Cyaxaress/User/Models/User.php
+++ modules/Cyaxaress/User/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace Cyaxaress\User\Models;
 
+use Cyaxaress\User\Notifications\VerifyMail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +37,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+
+    }
 }

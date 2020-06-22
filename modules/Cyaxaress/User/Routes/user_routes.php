b28c53122d2b2dd73f9f1ba3f9bad37008fb187a
Hemn Movafeghi <hemn791@gmail.com>
2020-06-22 12:47:03 +0300

    refactor user auth routes

--- modules/Cyaxaress/User/Routes/user_routes.php
+++ modules/Cyaxaress/User/Routes/user_routes.php
@@ -4,6 +4,27 @@ Route::group([
     'namespace' => 'Cyaxaress\User\Http\Controllers',
     'middleware' => 'web'
 ], function ($router) {
-    Auth::routes(['verify' => true]);
     Route::post('/email/verify', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('/email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+    Route::get('/email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+
+    // login
+    Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+    // logout
+    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+    // reset password
+    Route::get('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+    Route::post('/password/reset', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+    // register
+    Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+
+
 });

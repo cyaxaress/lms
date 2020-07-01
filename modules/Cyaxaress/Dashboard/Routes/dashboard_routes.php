c73a8611fc9974eddf7c2b843af7f65d1366e3f6
Hemn Movafeghi <hemn791@gmail.com>
2020-07-01 16:03:31 +0300

    Episode 51 - dashbaord module  middlewares

--- modules/Cyaxaress/Dashboard/Routes/dashboard_routes.php
+++ modules/Cyaxaress/Dashboard/Routes/dashboard_routes.php
@@ -1,4 +1,4 @@
 <?php
-Route::group(['namespace' => 'Cyaxaress\Dashboard\Http\Controllers'], function ($router) {
+Route::group(['namespace' => 'Cyaxaress\Dashboard\Http\Controllers', 'middleware' => ['web','auth', 'verified']], function ($router) {
     $router->get('/home', 'DashboardController@home')->name('home');
 });

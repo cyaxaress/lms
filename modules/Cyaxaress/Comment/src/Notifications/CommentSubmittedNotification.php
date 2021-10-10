5e2b332cfa72564db6866535bc84144fc31e51e1
Hemn Movafeghi <hemn791@gmail.com>
2021-10-10 14:47:32 +0300

    228- send telegram notifications

--- modules/Cyaxaress/Comment/src/Notifications/CommentSubmittedNotification.php
+++ modules/Cyaxaress/Comment/src/Notifications/CommentSubmittedNotification.php
@@ -3,10 +3,10 @@
 namespace Cyaxaress\Comment\Notifications;
 
 use Cyaxaress\Comment\Mail\CommentSubmittedMail;
-use Cyaxaress\User\Mail\ResetPasswordRequestMail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramChannel;
+use NotificationChannels\Telegram\TelegramMessage;
 
 class CommentSubmittedNotification extends Notification
 {
@@ -21,12 +21,28 @@ class CommentSubmittedNotification extends Notification
 
     public function via($notifiable): array
     {
-        return ['mail'];
+        return ['mail', TelegramChannel::class];
     }
 
     public function toMail($notifiable)
     {
         return (new CommentSubmittedMail($this->comment))->to($notifiable->email);
+    }
+
+    public function toTelegram($notifiable)
+    {
+        return TelegramMessage::create()
+            // Optional recipient user id.
+            ->to(118718802)
+            // Markdown supported.
+            ->content("یک دیدگاه جدید برای دوره ی شما در وب آموز ارسال شده است.")
+
+            // (Optional) Blade template for the content.
+            // ->view('notification', ['url' => $url])
+
+            // (Optional) Inline Buttons
+            ->button('مشاهده دوره', $this->comment->commentable->path())
+            ->button('مدیریت دیدگاه ها', route("comments.index"));
     }
 
     public function toArray($notifiable): array

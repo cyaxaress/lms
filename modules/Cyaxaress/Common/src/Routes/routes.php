af6dd8d48075490ddd3e94f0a09f3f605e1c28fa
Hemn Movafeghi <hemn791@gmail.com>
2022-03-13 18:56:08 +0300

    241- local repo to independent laravel package

--- modules/Cyaxaress/Common/src/Routes/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-Route::group(["middleware" => ["auth"]], function (Illuminate\Routing\Router $router){
-    $router->get("/notifications/mark-as-read", "NotificationController@markAllAsRead")
-        ->name("notifications.markAllAsRead");
-});

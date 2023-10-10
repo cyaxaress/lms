5108461453d82701d451f68001e382a0927cf02e
Hemn <hemn791@gmail.com>
2023-10-10 14:26:11 +0300

    remove test routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,1 @@
 <?php
-
-use Cyaxaress\Payment\Gateways\Gateway;
-
-Route::get('/test', function () {
-    event(new \Cyaxaress\Payment\Events\PaymentWasSuccessful(new \Cyaxaress\Payment\Models\Payment()));
-//    $gateway = resolve(Gateway::class);
-//    $payment = new \Cyaxaress\Payment\Models\Payment();
-//    $gateway->request($payment);
-
-//    \Spatie\Permission\Models\Permission::create(['name' => 'manage role_permissions']);
-//    auth()->user()->givePermissionTo(\Cyaxaress\RolePermissions\Models\Permission::PERMISSION_SUPER_ADMIN);
-//    return auth()->user()->assignRole('teacher');
-});
